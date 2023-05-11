5f52f027584df2d09c2b42eb652c4f5bb845d690
Hayden Buckley-Smith <hayden@abstack.co>
2023-05-11 17:07:10 +0100

    feat: create Character component

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,13 @@
+import Character from "./Character";
+
 function List() {
-  return <h2>Characters</h2>;
+  return (
+    <div>
+      <h2>Characters</h2>
+      <Character />
+      <Character />
+    </div>
+  );
 }
 
 export default List;
